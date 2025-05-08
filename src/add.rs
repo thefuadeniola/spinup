bc2f5526e0675fe4f5851e5928523db339de4a7b
Fuad Olaleye <leyefuad@gmail.com>
2025-05-08 11:02:54 +0100

    Completed add functionality

--- src/add.rs
+++ src/add.rs
@@ -1,10 +1,11 @@
-use std::fs;
+use std::{fs, env};
 use std::process::Command;
 use std::path::Path;
 
 pub fn add_program(name: &String) {
     let new_program = Path::new(name);
     let src = new_program.join("src");
+    let current_dir = env::current_dir().unwrap();
 
     fs::create_dir_all(&new_program).unwrap();
     fs::create_dir_all(&src).unwrap();
@@ -17,9 +18,67 @@ pub fn add_program(name: &String) {
     [dependencies]
     solana-program="1.9.0"
     "#, name);
+
+
+    let script = format!(
+        r#"
+    #! /bin/bash
     
+    SOLANA_PROGRAMS=("program", "{}")
+    
+    case $1 in
+        "reset")
+            rm -rf ./node_modules
+            for x in $(solana program show --programs | awk 'RP==0 {{print $1}}'); do 
+                if [[ $x != "Program" ]]; 
+                then 
+                    solana program close $x;
+                fi
+            done
+            for program in "${{SOLANA_PROGRAMS[@]}}"; do
+                cargo clean --manifest-path=./$program/Cargo.toml
+            done
+            rm -rf dist/program
+            ;;
+        "clean")
+            rm -rf ./node_modules
+            for program in "${{SOLANA_PROGRAMS[@]}}"; do
+                cargo clean --manifest-path=./$program/Cargo.toml
+            done;;
+        "build")
+            for program in "${{SOLANA_PROGRAMS[@]}}"; do
+                cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+            done;;
+        "deploy")
+            for program in "${{SOLANA_PROGRAMS[@]}}"; do
+                cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+                solana program deploy dist/program/$program.so
+            done;;
+        "reset-and-build")
+            rm -rf ./node_modules
+            for x in $(solana program show --programs | awk 'RP==0 {{print $1}}'); do 
+                if [[ $x != "Program" ]]; 
+                then 
+                    solana program close $x; 
+                fi
+            done
+            rm -rf dist/program
+            for program in "${{SOLANA_PROGRAMS[@]}}"; do
+                cargo clean --manifest-path=./$program/Cargo.toml
+                cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+                solana program deploy dist/program/$program.so
+            done
+            npm install
+            solana program show --programs
+            ;;
+    esac
+    "#,
+        name
+    );
+        
     fs::write(new_program.join("Cargo.toml"), cargo_toml_content.as_str()).unwrap();
     fs::write(src.join("lib.rs"), "").unwrap();
+    fs::write(current_dir.join("cicd.sh"), script).unwrap();
 
     Command::new("cargo")
             .arg("build")
--- src/new.rs
+++ src/new.rs
@@ -43,8 +43,11 @@ solana-program="1.9.0"
       "version": "1.0.0",
       "main": "main.ts",
       "scripts": {
-        "build": "tsc",
-        "start": "node main.ts"
+          "clean": "./cicd.sh clean",
+          "reset": "./cicd.sh reset",
+          "build": "./cicd.sh build",
+          "deploy": "./cicd.sh deploy",
+          "client": "ts-node ./client/main.ts"
       },
       "dependencies": {
         "@solana/web3.js": "^1.98.2",
@@ -58,25 +61,81 @@ solana-program="1.9.0"
     }
     "#;
 
-    
-    fs::write(program_path.join("Cargo.toml"), cargo_toml_content).unwrap();
-    fs::write(src.join("lib.rs"), "").unwrap();
-    fs::write(client.join("main.ts"), main_ts_content).unwrap();
-    fs::write(project_dir.join("package.json"), package_json).unwrap();
 
-    Command::new("npm")
-            .arg("install")
-            .current_dir(project_dir)
-            .status()
-            .expect("Failed to run npm install");
+    let script: &'static str =
+r#"
+#! /bin/bash
 
-    Command::new("cargo")
-            .arg("build")
-            .current_dir(program_path)
-            .status()
-            .expect("Failed to run cargo build");
+SOLANA_PROGRAMS=("program")
+
+case $1 in
+    "reset")
+        rm -rf ./node_modules
+        for x in $(solana program show --programs | awk 'RP==0 {print $1}'); do 
+            if [[ $x != "Program" ]]; 
+            then 
+                solana program close $x;
+            fi
+        done
+        for program in "${SOLANA_PROGRAMS[@]}"; do
+            cargo clean --manifest-path=./$program/Cargo.toml
+        done
+        rm -rf dist/program
+        ;;
+    "clean")
+        rm -rf ./node_modules
+        for program in "${SOLANA_PROGRAMS[@]}"; do
+            cargo clean --manifest-path=./$program/Cargo.toml
+        done;;
+    "build")
+        for program in "${SOLANA_PROGRAMS[@]}"; do
+            cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+        done;;
+    "deploy")
+        for program in "${SOLANA_PROGRAMS[@]}"; do
+            cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+            solana program deploy dist/program/$program.so
+        done;;
+    "reset-and-build")
+        rm -rf ./node_modules
+        for x in $(solana program show --programs | awk 'RP==0 {print $1}'); do 
+            if [[ $x != "Program" ]]; 
+            then 
+                solana program close $x; 
+            fi
+        done
+        rm -rf dist/program
+        for program in "${SOLANA_PROGRAMS[@]}"; do
+            cargo clean --manifest-path=./$program/Cargo.toml
+            cargo build-bpf --manifest-path=./$program/Cargo.toml --bpf-out-dir=./dist/program
+            solana program deploy dist/program/$program.so
+        done
+        npm install
+        solana program show --programs
+        ;;
+esac
+"#; 
+
+  fs::write(program_path.join("Cargo.toml"), cargo_toml_content).unwrap();
+  fs::write(src.join("lib.rs"), "").unwrap();
+  fs::write(client.join("main.ts"), main_ts_content).unwrap();
+  fs::write(project_dir.join("package.json"), package_json).unwrap();
+  fs::write(project_dir.join("cicd.sh"), script).unwrap();
+
+  Command::new("npm")
+          .arg("install")
+          .current_dir(project_dir)
+          .status()
+          .expect("Failed to run npm install");
+
+  Command::new("cargo")
+          .arg("build")
+          .current_dir(program_path)
+          .status()
+          .expect("Failed to run cargo build");
 
 
-    println!("Project {} created! Run 'code .' to open it in your IDE", name);
+  println!("Project {} created! Run 'code .' to open it in your IDE", name);
+
 
 }
